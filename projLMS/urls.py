47140bacfa09961fd94e1b1cf918b2306f4059fb
Rick Gonçalves Veras <42049142+Rickein@users.noreply.github.com>
2018-11-08 21:23:45 -0200

    Update urls.py

--- projLMS/urls.py
+++ projLMS/urls.py
@@ -10,14 +10,5 @@ urlpatterns = [
     path('noticias/',noticias),
     path('noticia1/',noticia1),
     path('admin/', admin.site.urls),
-    path('disciplina_bd/', disciplina_bd)
-    path('disciplina_ads/', disciplina_ads)
-    path('disciplina_adm/', disciplina_adm)
-    path('disciplina_gestaoti/', disciplina_gestaoti)
-    path('disciplina_jogo/', disciplina_jogo)
-    path('disciplina_producao_multimidia/', disciplina_producao_multimidia)
-    path('disciplina_redes/', disciplina_redes)
-    path('disciplina_si/', disciplina_si)
-    path('disciplina_sociedade_sustentabilidade/', disciplina_sociedade_sustentabilidade)
 ]
 
